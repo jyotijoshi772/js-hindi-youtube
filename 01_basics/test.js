8d32fe479ae572c65ebb518f0cc2cbb8d90141a8
jyotijoshi772 <jyotijoshi772@gmail.com>
2024-06-30 17:43:53 +0000

    chnages in test.js

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,37 @@
 console.log("jyoti")
+
+// premitive datatypes(7)
+
+// String
+// Number
+// Boolean
+// Undefined
+// null
+// 
+// 
+
+// Non premitive datatype(3)
+
+// Array, Object, Function
+
+const Id = 87
+console.log(typeof Id)
+
+let myName = "jyoti"
+var myClass = 10
+myStatus = true
+
+console.table([myClass, myName, myStatus])
+
+let string1 = "hello"
+let string2 = "jyoti"
+
+final = string1 + string2
+console.log(final)
+console.log(`my first name is ${string1} and last name is ${string2}`)
+
+proto = final.__proto__
+console.log(proto)
+console.log(string1.toUpperCase())
+console.log(string1.toLowerCase())
+console.log(final.split("o"))
